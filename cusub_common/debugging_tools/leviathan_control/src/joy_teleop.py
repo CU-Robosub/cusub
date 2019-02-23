a2ecb8e59cce56c3dca7259956785cc7cfa1d3e6
Michael Marques <mima3079@colorado.edu>
2019-02-23 10:41:08 -0700

    Changed Joystick to a single launch for direct/setpoint mode Moved direct joystick controller matrix into controller matrix Created thrust odom to try to stabalize the EKF Added linear dampening to stablize Triton Adjusted Triton camera to closer to final parameters

--- cusub_common/debugging_tools/leviathan_control/src/joy_teleop.py
+++ cusub_common/debugging_tools/leviathan_control/src/joy_teleop.py
@@ -1,130 +1,170 @@
 #!/usr/bin/env python
-# license removed for brevity
-import rospy
+"""
+Allows joystick teleoperation of submarine
+"""
+
 import math
+
+import rospy
+
 from std_msgs.msg import Float64
 from sensor_msgs.msg import Joy
 
-STRAFE_AXES  = 0
-DRIVE_AXES = 1
-YAW_AXES = 2
-DEPTH_AXES = 3
+from actuator.srv import ActivateActuator
 
-DEFAULT_DEPTH = 0
+class JoyTeleop(object):
 
-strafe_val  = 0.0
-drive_val = 0.0
-yaw_val = 0.0
-depth_val = 0.0
-pitch_val = 0.0
-roll_val = 0.0
+    strafe_axes  = 0
+    drive_axes = 1
+    yaw_axes = 2
+    depth_axes = 3
+    pitch_axes = 2
+    roll_axes = 2
 
-depth_f64 = None
+    default_depth = 0
 
-TWIST_EFFORT    = 1.0
-TRANLATE_EFFORT = 1.0
+    strafe_val  = 0.0
+    drive_val = 0.0
+    yaw_val = 0.0
+    depth_val = 0.0
+    pitch_val = 0.0
+    roll_val = 0.0
 
-THRUSTER_POWER = 75.0
+    depth_f64 = None
 
-def joystick_state(data):
+    twist_effort    = 1.0
+    translate_effort = 1.0
 
-    global STRAFE_AXES
-    global DRIVE_AXES
-    global YAW_AXES
-    global DEPTH_AXES
-    global PITCH_AXES
-    global ROLL_AXES
+    thruster_power = 75.0
 
-    global strafe_val
-    global drive_val
-    global yaw_val
-    global depth_val
-    global pitch_val
-    global roll_val
+    yaw_sensitivity = 0.02
+    drive_sensitivity = 0.02
+    strafe_sensitivity = 0.02
+    depth_sensitivity = 0.02
 
-    strafe_val  = data.axes[STRAFE_AXES]
-    drive_val = data.axes[DRIVE_AXES]
-    yaw_val = data.axes[YAW_AXES]
-    depth_val = data.axes[DEPTH_AXES]
-    pitch_val = data.axes[PITCH_AXES]
-    roll_val = data.axes[ROLL_AXES]
+    dropper_triggered = False
 
-def joy_teleop():
+    def joystick_state(self, data):
+        """Gets joystick data to figure out what to have the sub do
+        """
 
-    global STRAFE_AXES
-    global DRIVE_AXES
-    global YAW_AXES
-    global DEPTH_AXES
-    global PITCH_AXES
-    global ROLL_AXES
+        if(data.buttons[0]):
+            self.dropper_triggered
 
-    global DEFAULT_DEPTH
+        self.strafe_val  = data.axes[self.strafe_axes]
+        self.drive_val = data.axes[self.drive_axes]
+        self.yaw_val = data.axes[self.yaw_axes]
+        self.depth_val = data.axes[self.depth_axes]
+        self.pitch_val = data.axes[self.pitch_axes]
+        self.roll_val = data.axes[self.roll_axes]
 
-    global strafe_val
-    global drive_val
-    global yaw_val
-    global depth_val
-    global pitch_val
-    global roll_val
+    def joy_teleop(self):
 
-    pub_yaw = rospy.Publisher('/local_control/pid/yaw/control_effort', Float64, queue_size=10)
-    pub_drive = rospy.Publisher('/local_control/pid/drive/control_effort', Float64, queue_size=10)
-    pub_strafe = rospy.Publisher('/local_control/pid/strafe/control_effort', Float64, queue_size=10)
+        rospy.init_node('joy_teleop', anonymous=True)
 
-    pub_pitch = rospy.Publisher('/local_control/pid/pitch/setpoint', Float64, queue_size=10)
-    pub_roll = rospy.Publisher('/local_control/pid/roll/setpoint', Float64, queue_size=10)
+        namespace = rospy.get_param('~namespace')
 
-    pub_depth = rospy.Publisher('/local_control/pid/depth/setpoint', Float64, queue_size=10)
+        setpoint = rospy.get_param('~setpoint', True)
 
-    rospy.init_node('joy_teleop', anonymous=True)
+        if setpoint:
 
-    STRAFE_AXES = rospy.get_param("~strafe_axes", STRAFE_AXES)
-    DRIVE_AXES = rospy.get_param("~drive_axes", DRIVE_AXES)
-    YAW_AXES = rospy.get_param("~yaw_axes", YAW_AXES)
-    DEPTH_AXES = rospy.get_param("~depth_axes", DEPTH_AXES)
-    PITCH_AXES = rospy.get_param("~pitch_axes", YAW_AXES)
-    ROLL_AXES = rospy.get_param("~roll_axes", YAW_AXES)
+            pub_yaw = rospy.Publisher(namespace + '/local_control/pid/yaw/setpoint',
+                                      Float64, queue_size=10)
+            pub_drive = rospy.Publisher(namespace + '/local_control/pid/drive/setpoint',
+                                        Float64, queue_size=10)
+            pub_strafe = rospy.Publisher(namespace + '/local_control/pid/strafe/setpoint',
+                                         Float64, queue_size=10)
 
-    DEFAULT_DEPTH = rospy.get_param("~default_depth", DEFAULT_DEPTH)
+        else: 
 
-    depth_f64 = Float64()
-    depth_f64.data = DEFAULT_DEPTH
+            pub_yaw = rospy.Publisher(namespace + '/local_control/mux/yaw/control_effort',
+                                      Float64, queue_size=10)
+            pub_drive = rospy.Publisher(namespace + '/local_control/mux/drive/control_effort',
+                                         Float64, queue_size=10)
+            pub_strafe = rospy.Publisher(namespace + '/local_control/mux/strafe/control_effort',
+                                         Float64, queue_size=10)
 
-    pitch_f64 = Float64()
-    pitch_f64.data = 0
-    roll_f64 = Float64()
-    roll_f64.data = 0
+        pub_pitch = rospy.Publisher(namespace + '/local_control/pid/pitch/setpoint',
+                                    Float64, queue_size=10)
+        pub_roll = rospy.Publisher(namespace + '/local_control/pid/roll/setpoint',
+                                   Float64, queue_size=10)
 
-    rospy.Subscriber("/joy", Joy, joystick_state)
+        pub_depth = rospy.Publisher(namespace + '/local_control/pid/depth/setpoint',
+                                    Float64, queue_size=10)
 
-    rate = rospy.Rate(30)
+        self.strafe_axes = rospy.get_param("~strafe_axes", self.strafe_axes)
+        self.drive_axes = rospy.get_param("~drive_axes", self.drive_axes)
+        self.yaw_axes = rospy.get_param("~yaw_axes", self.yaw_axes)
+        self.depth_axes = rospy.get_param("~depth_axes", self.depth_axes)
+        self.pitch_axes = rospy.get_param("~pitch_axes", self.pitch_axes)
+        self.roll_axes = rospy.get_param("~roll_axes", self.roll_axes)
 
-    while not rospy.is_shutdown():
+        self.default_depth = rospy.get_param("~default_depth", self.default_depth)
 
-        yaw_f64 = Float64()
-        yaw_f64.data = yaw_val*THRUSTER_POWER*TWIST_EFFORT
-        pub_yaw.publish(yaw_f64)
+        depth_f64 = Float64()
+        depth_f64.data = self.default_depth
 
         drive_f64 = Float64()
-        drive_f64.data = drive_val*THRUSTER_POWER
-        pub_drive.publish(drive_f64)
-
+        drive_f64.data = 0.0
         strafe_f64 = Float64()
-        strafe_f64.data = -1*strafe_val*THRUSTER_POWER
-        pub_strafe.publish(strafe_f64)
+        strafe_f64.data = 0.0
+        yaw_f64 = Float64()
+        yaw_f64.data = 0.0
 
-        depth_f64.data = depth_f64.data + depth_val/10.0
-        pub_depth.publish(depth_f64)
+        pitch_f64 = Float64()
+        pitch_f64.data = 0
+        roll_f64 = Float64()
+        roll_f64.data = 0
 
-        #pitch_f64.data = pitch_val*math.radians(15.0) # allow 15 deg pitch
-        #pub_pitch.publish(pitch_f64)
-        #roll_f64.data = roll_val*math.radians(15.0) # allow 15 deg roll
-        #pub_roll.publish(roll_f64)
+        rospy.Subscriber("/joy", Joy, self.joystick_state)
 
-        rate.sleep()
+        rate = rospy.Rate(30)
+
+        while not rospy.is_shutdown():
+
+            if setpoint:
+
+                yaw_f64.data = yaw_f64.data + self.yaw_val * self.yaw_sensitivity
+                pub_yaw.publish(yaw_f64)
+
+                drive_f64.data = drive_f64.data + self.drive_val * self.drive_sensitivity
+                pub_drive.publish(drive_f64)
+
+                strafe_f64.data = strafe_f64.data - self.strafe_val * self.strafe_sensitivity
+                pub_strafe.publish(strafe_f64)
+
+            else:
+
+                yaw_f64 = Float64()
+                yaw_f64.data = self.yaw_val * self.thruster_power * self.twist_effort
+                pub_yaw.publish(yaw_f64)
+
+                drive_f64 = Float64()
+                drive_f64.data = self.drive_val * self.thruster_power
+                pub_drive.publish(drive_f64)
+
+                strafe_f64 = Float64()
+                strafe_f64.data = -1 * self.strafe_val * self.thruster_power
+                pub_strafe.publish(strafe_f64)
+
+            depth_f64.data = depth_f64.data + self.depth_val * self.depth_sensitivity
+            pub_depth.publish(depth_f64)
+
+            pitch_f64.data = self.pitch_val*math.radians(15.0) # allow 15 deg pitch
+            pub_pitch.publish(pitch_f64)
+            roll_f64.data = self.roll_val*math.radians(15.0) # allow 15 deg roll
+            pub_roll.publish(roll_f64)
+
+            if(self.dropper_triggered):
+                activate_actuator = rospy.ServiceProxy('/activateActuator', ActivateActuator)
+                activate_actuator(1, 100) # Activate actuator 1 for 100 seconds
+                self.dropper_triggered = False
+
+            rate.sleep()
 
 if __name__ == '__main__':
+    JOY_TELEOP = JoyTeleop()
     try:
-        joy_teleop()
+        JOY_TELEOP.joy_teleop()
     except rospy.ROSInterruptException:
-        rospy.loginfo("Mother fucker!")
+        rospy.loginfo("Joy Setpoint Teleop has died!")
