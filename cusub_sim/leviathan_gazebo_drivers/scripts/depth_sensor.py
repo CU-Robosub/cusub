40af172c9a74dc63486424bcd748852c235a26ac
Michael Marques <mima3079@colorado.edu>
2019-02-26 18:07:23 -0700

    Fixed merge mistakes

--- cusub_sim/leviathan_gazebo_drivers/scripts/depth_sensor.py
+++ cusub_sim/leviathan_gazebo_drivers/scripts/depth_sensor.py
@@ -1,58 +1,81 @@
 #!/usr/bin/env python
+"""
+This module facilitates republishing of the depth sensor fluid pressure
+from gazebo into a depth pose for sensor fusion
+"""
 
 import rospy
 
 from sensor_msgs.msg import FluidPressure
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
-depth_pose = None
+class DepthSensor(object):
+    """
+    This node republishes of the depth sensor fluid pressure
+    from gazebo into a depth pose for sensor fusion
+    """
 
-def pressure_callback(data):
+    depth_pose = None
+    """PoseWithCovarianceStamped : Current depth pose"""
 
-    global depth_pose
+    # Bluerobotics Bar30 Pressure Sensor resolution is 2mm
+    resolution_m = 0.002
+    """float : Resolution of depths sensor in meters"""
 
-    depth_pose.header.stamp = rospy.Time.now()
-    depth_pose.header.seq += 1
+    # TODO needs calibration for gazebo, or we can just go with
+    #  the current method of using the depth sensor driver to
+    #  calibrate a zero depth
+    pressure_offset = 103.2
+    """pressure offset to correct pressure to depth at surface"""
 
-    # 103.2 = 8.748m
+    def pressure_callback(self, data):
+        """Gets current pressure from gazebo"""
 
-    depth = (data.fluid_pressure - 103.2) / 9.91
+        self.depth_pose.header.stamp = rospy.Time.now()
+        self.depth_pose.header.seq += 1
 
-    # make depth resolution 8.1cm
-    depth = round(depth / 0.081) * 0.081 - 0.85
+        # 103.2 = 8.748m
 
-    depth_pose.pose.pose.position.z = depth
+        depth = (data.fluid_pressure - self.pressure_offset) / 9.91
 
-def depth_sensor():
+        # create stepped depth resolution to match real sensor
+        depth = round(depth / self.resolution_m) * self.resolution_m
 
-    global depth_pose
+        # -1 Z up
+        self.depth_pose.pose.pose.position.z = -1.0*depth
 
-    rospy.init_node('depth_sensor_gazebo', anonymous=True)
-    rospy.Subscriber("/leviathan/pressure", FluidPressure, pressure_callback)
+    def depth_sensor(self):
+        """Republishes presure as depth pose"""
 
-    depth_pose = PoseWithCovarianceStamped()
-    depth_pose.pose.covariance = [0, 0, 0, 0, 0, 0,
-					      0, 0, 0, 0, 0, 0,
-					      0, 0, 0.01 , 0, 0, 0,
-					      0, 0, 0, 0, 0, 0,
-					      0, 0, 0, 0, 0, 0,
-					      0, 0, 0, 0, 0, 0]
-    depth_pose.header.frame_id = "depth_frame"
-    depth_pose.pose.pose.position.z = 0
+        rospy.init_node('depth_sensor_gazebo', anonymous=True)
 
-    depth_pub = rospy.Publisher('depth', PoseWithCovarianceStamped, queue_size=1)
+        namespace = rospy.get_param('~namespace')
 
-    rate = rospy.Rate(30)
+        rospy.Subscriber(namespace + '/pressure', FluidPressure, self.pressure_callback)
 
-    while not rospy.is_shutdown():
+        self.depth_pose = PoseWithCovarianceStamped()
+        self.depth_pose.pose.covariance = [0, 0, 0, 0, 0, 0,
+                                           0, 0, 0, 0, 0, 0,
+                                           0, 0, 0.01, 0, 0, 0,
+                                           0, 0, 0, 0, 0, 0,
+                                           0, 0, 0, 0, 0, 0,
+                                           0, 0, 0, 0, 0, 0]
+        self.depth_pose.header.frame_id = namespace + '/depth_frame'
+        self.depth_pose.pose.pose.position.z = 0
 
-	#depth_pose.header.stamp = rospy.Time.now()
-        depth_pub.publish(depth_pose)
+        depth_pub = rospy.Publisher(namespace + '/depth', PoseWithCovarianceStamped, queue_size=1)
 
-        rate.sleep()
+        rate = rospy.Rate(30)
+
+        while not rospy.is_shutdown():
+
+            depth_pub.publish(self.depth_pose)
+
+            rate.sleep()
 
 if __name__ == '__main__':
+    DEPTH_SENSOR = DepthSensor()
     try:
-        depth_sensor()
+        DEPTH_SENSOR.depth_sensor()
     except rospy.ROSInterruptException:
-        rospy.loginfo("Mother fucker!")
+        rospy.loginfo("Depth sensor died!")
