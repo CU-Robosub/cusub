b1d503c0fbf95fd107e00b9dff650e92faf2b158
Luke Barbier <lukebarbier12@gmail.com>
2019-03-27 17:00:42 -0600

    sim object pose publisher

--- cusub_cortex/mapper/scripts/mapper.py
+++ cusub_cortex/mapper/scripts/mapper.py
@@ -7,6 +7,8 @@ the global frame.
 Eventually, this may be replaced by mapper_server in order to use actionlib
 
 NOTE: the unfiltered poses are left in for visual debugging purposes
+
+There must be a cleaner way to do this than all of these if statements...
 """
 
 import rospy
@@ -14,26 +16,25 @@ import tf
 from geometry_msgs.msg import PoseArray, Pose, PoseStamped
 from localizer.msg import Detection
 
-from pose_filter import filter_poses
-
 class Mapper():
     def __init__(self):
 
         self.namespace = rospy.get_param('~namespace_odom')
-        ns = rospy.get_namespace()
-        index = ns[1:].find('/') # find the 2nd '/'
-        sub_name = ns[0:index+2]
-        print(sub_name)
         
-        # new
-        self.pose_sub = rospy.Subscriber(sub_name + 'cusub_perception/mapper/task_poses', Detection, self.pose_received)
+        self.listener = tf.TransformListener(interpolate=True, cache_time=rospy.Duration(1)) #, cache_time=rospy.Duration(1))       
+        # ns = rospy.get_namespace()
+        # index = ns[1:].find('/') # find the 2nd '/'
+        # sub_name = ns[0:index+2]
+        # print(sub_name)
+        
+        rospy.Subscriber('cusub_cortex/mapper_in/task_poses', Detection, self.pose_received)
 
         # separate topics for each task obstacle
         # dice
-        self.dice1_pub = rospy.Publisher(ns + 'mapper/dice1_pose', PoseStamped, queue_size=10)
-        self.dice2_pub = rospy.Publisher(ns + 'mapper/dice2_pose', PoseStamped, queue_size=10)
-        self.dice5_pub = rospy.Publisher(ns + 'mapper/dice5_pose', PoseStamped, queue_size=10)
-        self.dice6_pub = rospy.Publisher(ns + 'mapper/dice6_pose', PoseStamped, queue_size=10)
+        self.dice1_pub = rospy.Publisher('cusub_cortex/mapper_out/dice1', PoseStamped, queue_size=10)
+        self.dice2_pub = rospy.Publisher('cusub_cortex/mapper_out/dice2', PoseStamped, queue_size=10)
+        self.dice5_pub = rospy.Publisher('cusub_cortex/mapper_out/dice5', PoseStamped, queue_size=10)
+        self.dice6_pub = rospy.Publisher('cusub_cortex/mapper_out/dice6', PoseStamped, queue_size=10)
 
         self.dice1_unfiltered_pub = rospy.Publisher('cusub_cortex/unfiltered/dice1_pose', PoseStamped, queue_size=10)
         self.dice2_unfiltered_pub = rospy.Publisher('cusub_cortex/unfiltered/dice2_pose', PoseStamped, queue_size=10)
@@ -56,7 +57,7 @@ class Mapper():
         self.dice6_found = False
 
         # start gate
-        self.start_gate_pub = rospy.Publisher(ns + 'mapper/start_gate', PoseStamped, queue_size=10)
+        self.start_gate_pub = rospy.Publisher('cusub_cortex/mapper_out/start_gate', PoseStamped, queue_size=10)
 
         self.start_gate_unfiltered_pub = rospy.Publisher('cusub_cortex/unfiltered/start_gate', PoseStamped, queue_size=10)
 
@@ -65,12 +66,6 @@ class Mapper():
         self.start_gate_unfiltered_pose = PoseStamped()
 
         self.start_gate_found = False
-
-        # filters
-        self.gate_filter = filter_poses.GatePoseFilter()
-        self.dice_filter = filter_poses.DicePoseFilter()
-
-
 
         #TODO: this should be dynamic, and corresponding transform from occam to odom should happen
         self.dice1_unfiltered_pose.header.frame_id = 'occam0_frame'
@@ -82,59 +77,45 @@ class Mapper():
 
 
         # tranforms
-        self.listener = tf.TransformListener()
+
 
     def pose_received(self, detection):
-        pose = PoseStamped()
-        pose.pose = detection.location
-        pose.header.stamp = detection.image_header.stamp
-        pose.header.frame_id = detection.camera_frame
-
-        transformed_pose = self.transform_occam_to_odom(pose)
-
-        object = detection.object_type
-
-        # TODO: change time to detection's time
-        if object == 'dice1':
+        
+        transformed_pose = self.transform_occam_to_odom(detection.pose)
+        
+        if transformed_pose == None:
+            pass
+        elif detection.class_id == 'dice1':
             self.dice1_unfiltered_pose = transformed_pose
-            self.dice1_pose = self.dice_filter.send_pose(transformed_pose)
+            self.dice1_pose = transformed_pose
             self.dice1_found = True
 
-        elif object == 'dice2':
+        elif detection.class_id == 'dice2':
             self.dice2_unfiltered_pose = transformed_pose
-            self.dice2_pose = self.dice_filter.send_pose(transformed_pose)
+            self.dice2_pose = transformed_pose
             self.dice2_found = True
 
-        elif object == 'dice5':
+        elif detection.class_id == 'dice5':
             self.dice5_unfiltered_pose = transformed_pose
-            self.dice5_pose = self.dice_filter.send_pose(transformed_pose)
+            self.dice5_pose = transformed_pose
             self.dice5_found = True
 
-        elif object == 'dice6':
+        elif detection.class_id == 'dice6':
             self.dice6_unfiltered_pose = transformed_pose
-            self.dice6_pose = self.dice_filter.send_pose(transformed_pose)
+            self.dice6_pose = transformed_pose 
             self.dice6_found = True
 
-        elif object == 'start_gate':
+        elif detection.class_id == 'start_gate':
             self.start_gate_unfiltered_pose = transformed_pose
-            self.start_gate_pose = self.gate_filter.send_pose(transformed_pose)
+            self.start_gate_pose = transformed_pose 
             self.start_gate_found = True
 
-
-
-    # occam_pose is PoseStamped with frame id being the occam frame it came from
-    # i.e. occam0_frame, occam1_frame, occam4_frame
     def transform_occam_to_odom(self, occam_pose):
-        # below needed ?
-        # now = rospy.Time(0)
-        # self.listener.waitForTransform('odom', 'base_link', now, rospy.Duration(1))
-        # occam_pose.header.stamp = now
-
-        # Gotta love tf
-        # must be aboslute reference for inter meta-package
-        odom_pose = self.listener.transformPose('/' + self.namespace + '/odom', occam_pose)
-        return odom_pose
-
+        try:
+            pose = self.listener.transformPose('/' + self.namespace + '/odom', occam_pose)
+        except (tf.ExtrapolationException, tf.ConnectivityException, tf.LookupException) as e:
+            pose = None
+        return pose
 
     def publish_poses(self):
         r = rospy.Rate(10) # 10 Hz
@@ -155,7 +136,6 @@ class Mapper():
             if self.dice6_found:
                 self.dice6_pub.publish(self.dice6_pose)
                 self.dice6_unfiltered_pub.publish(self.dice6_unfiltered_pose)
-
 
             if self.start_gate_found:
                 self.start_gate_pub.publish(self.start_gate_pose)
