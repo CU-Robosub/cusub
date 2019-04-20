366e4141587b9b69d7222c2b0e3694e6241e662c
Michael Marques <mima3079@colorado.edu>
2019-04-20 15:11:11 -0600

    Changed point finding to contour polygon approximation in roulette watershed

--- cusub_perception/localizer/scripts/roulette_servers/roulette_watershed_srv.py
+++ cusub_perception/localizer/scripts/roulette_servers/roulette_watershed_srv.py
@@ -17,6 +17,13 @@ import cv2
 
 class RouletteWatershedServer():
 
+    METHOD_MAXMIN = 1
+    METHOD_CONTOUR = 2
+
+    method = METHOD_CONTOUR
+
+    debug = False
+
     # 2-3
     # | |
     # 1 4
@@ -42,10 +49,87 @@ class RouletteWatershedServer():
 
         self.bridge = CvBridge()
 
+    def order_points(self, points):
+
+        l1 = np.linalg.norm(np.array(points[0]) - np.array(points[1]))
+        l2 = np.linalg.norm(np.array(points[2]) - np.array(points[1]))
+
+        pr_arr = None
+        if(l2 > l1):
+            # top to right is long side
+            pt_arr = np.array([points[3], points[0], points[1], points[2]], dtype=np.float32)
+        else:
+            # top to left is long side
+            pt_arr = np.array([points[0], points[1], points[2], points[3]], dtype=np.float32)
+
+        return pt_arr
+
+    def points_by_max_minima(self, markers, debug_img):
+        """
+        This function returns the points for the roulette outline
+        by looking at left right top bottom maximums
+        """
+
+        marker_pos = np.argwhere(markers == 2)
+
+        marker_xmin_idx = np.argmin(marker_pos[:,0])
+        marker_xmin_x = marker_pos[marker_xmin_idx,0]
+        marker_xmin_y = marker_pos[marker_xmin_idx,1]
+        marker_left = (marker_xmin_y, marker_xmin_x)
+
+        marker_xmax_idx = np.argmax(marker_pos[:,0])
+        marker_xmax_x = marker_pos[marker_xmax_idx,0]
+        marker_xmax_y = marker_pos[marker_xmax_idx,1]
+        marker_right = (marker_xmax_y, marker_xmax_x)
+
+        marker_ymin_idx = np.argmin(marker_pos[:,1])
+        marker_ymin_x = marker_pos[marker_ymin_idx,0]
+        marker_ymin_y = marker_pos[marker_ymin_idx,1]
+        marker_top = (marker_ymin_y, marker_ymin_x)
+
+        marker_ymax_idx = np.argmax(marker_pos[:,1])
+        marker_ymax_x = marker_pos[marker_ymax_idx,0]
+        marker_ymax_y = marker_pos[marker_ymax_idx,1]
+        marker_bottom = (marker_ymax_y, marker_ymax_x)
+
+        pt_arr = self.order_points([marker_left, marker_top, marker_right, marker_bottom])
+
+        if debug_img is not None:
+            pt_arr_2 = tuple(map(tuple, pt_arr))
+            cv2.circle(debug_img, pt_arr_2[0], 5, (0,0,255), -1)
+            cv2.circle(debug_img, pt_arr_2[1], 5, (0,0,255), -1)
+            cv2.circle(debug_img, pt_arr_2[2], 5, (0,0,255), -1)
+            cv2.circle(debug_img, pt_arr_2[3], 5, (0,0,255), -1)
+
+        return pt_arr
+
+    def get_points_by_contour(self, markers, debug_img):
+        """
+        This functino returns points of the roulette by
+        taking the contour and finding the polygon approximation
+        """
+
+        ret, thresh = cv2.threshold(np.uint8(markers), 127, 255, 0)
+        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        epsilon = 0.1*cv2.arcLength(contours[2],True)
+        approx = cv2.approxPolyDP(contours[2], epsilon, True)
+
+        pt_arr = self.order_points(approx)
+
+        if debug_img is not None:
+            #cv2.drawContours(debug_img, contours, -1, (0,255,0), 3)
+            cv2.polylines(debug_img,[approx],True,(0,255,255))
+
+        return pt_arr
+
     def localize(self, req):
 
         img = self.bridge.imgmsg_to_cv2(req.image, "passthrough")
-        #debug_img = img.copy()
+
+        debug_img = None
+        if self.debug:
+            debug_img = img.copy()
 
         # Fill Background
         markers = np.ones((req.image.height, req.image.width), dtype=np.int32)
@@ -79,50 +163,26 @@ class RouletteWatershedServer():
 
             marker_count += 1
 
-        if marker_count == 2: # noting found
+        if marker_count == 2: # nothing found
             return None, None
 
         cv2.watershed(img, markers)
 
-        marker_pos = np.argwhere(markers == 2)
-
-        marker_xmin_idx = np.argmin(marker_pos[:,0])
-        marker_xmin_x = marker_pos[marker_xmin_idx,0]
-        marker_xmin_y = marker_pos[marker_xmin_idx,1]
-        marker_left = (marker_xmin_y, marker_xmin_x)
-
-        marker_xmax_idx = np.argmax(marker_pos[:,0])
-        marker_xmax_x = marker_pos[marker_xmax_idx,0]
-        marker_xmax_y = marker_pos[marker_xmax_idx,1]
-        marker_right = (marker_xmax_y, marker_xmax_x)
-
-        marker_ymin_idx = np.argmin(marker_pos[:,1])
-        marker_ymin_x = marker_pos[marker_ymin_idx,0]
-        marker_ymin_y = marker_pos[marker_ymin_idx,1]
-        marker_top = (marker_ymin_y, marker_ymin_x)
-
-        marker_ymax_idx = np.argmax(marker_pos[:,1])
-        marker_ymax_x = marker_pos[marker_ymax_idx,0]
-        marker_ymax_y = marker_pos[marker_ymax_idx,1]
-        marker_bottom = (marker_ymax_y, marker_ymax_x)
-
-        l1 = np.linalg.norm(np.array(marker_left)  - np.array(marker_top))
-        l2 = np.linalg.norm(np.array(marker_right) - np.array(marker_top))
-
-        # print(l1, l2)
-
-        # print(marker_left, marker_right, marker_top, marker_bottom)
-
-        # 2-3
-        # | |
-        # 1 4
-        pr_arr = None
-        if(l2 > l1):
-            # top to right is long side
-            pt_arr = np.array([marker_bottom, marker_left, marker_top, marker_right], dtype=np.float32)
+        if self.method == self.METHOD_MAXMIN:
+            pt_arr = self.points_by_max_minima(markers, debug_img)
+        elif self.method == self.METHOD_CONTOUR:
+            pt_arr = self.get_points_by_contour(markers, debug_img)
         else:
-            # top to left is long side
-            pt_arr = np.array([marker_left, marker_top, marker_right, marker_bottom], dtype=np.float32)
+            # Fail
+            return None, None
+
+        if pt_arr is None:
+            # Fail
+            return None, None
+
+        if len(pt_arr) != 4:
+            # Fail
+            return None, None
 
         retval, rvec, tvec = cv2.solvePnP(self.roulette_truth_points, pt_arr, self.downcam_camera_matrix, self.downcam_distortion_coefs)
 
@@ -134,7 +194,8 @@ class RouletteWatershedServer():
 
         poses.append(pose)
 
-        # print(pose)
+        if self.debug:
+          print(pose)
 
         box = req.boxes[0]
         classes.append(box.Class)
@@ -150,16 +211,9 @@ class RouletteWatershedServer():
 
         # cv2.imshow('image', markers)
 
-        """
-        cv2.circle(debug_img, marker_left, 5, (0,0,255), -1)
-        cv2.circle(debug_img, marker_right, 5, (0,0,255), -1)
-        cv2.circle(debug_img, marker_bottom, 5, (0,0,255), -1)
-        cv2.circle(debug_img, marker_top, 5, (0,0,255), -1)
-
-        cv2.imshow('image', debug_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        """
+        if self.debug:
+            cv2.imshow('Roulette Watershed', debug_img)
+            cv2.waitKey(1)
 
         return poses, classes
 
@@ -182,6 +236,7 @@ def test1():
     image_message = bridge.cv2_to_imgmsg(cv_image, "passthrough")
 
     roulette_box = BoundingBox()
+    roulette_box.Class = "roulette"
     roulette_box.xmin = 793
     roulette_box.ymin = 415
     roulette_box.xmax = 1000
@@ -192,6 +247,9 @@ def test1():
     req.boxes.append(roulette_box)
 
     server.localize(req)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     # test1()
