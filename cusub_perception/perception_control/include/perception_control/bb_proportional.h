c09deca5af7ae63a4583813fb7ec93be42593593
Luke <lukebarbier12@gmail.com>
2019-07-15 23:42:57 -0600

    Proportional controller loads params

--- cusub_perception/perception_control/include/perception_control/bb_proportional.h
+++ cusub_perception/perception_control/include/perception_control/bb_proportional.h
@@ -2,6 +2,7 @@
 #define BB_PROPORTIONAL_CLASS_SRC_BB_PROPORTIONAL_CLASS_H_
 
 #include <perception_control/bb_controller.h>
+#include <string>
 
 namespace perception_control
 {
@@ -10,6 +11,9 @@ namespace perception_control
     public:
         void respond(int xdiff, int ydiff);
         BBProportional(ros::NodeHandle& nh);
+    private:
+        float downcam_drive_max_setpoint, downcam_strafe_max_setpoint;
+        int downcam_drive_maxout_pixel_dist, downcam_strafe_maxout_pixel_dist; 
     };
 }
 
