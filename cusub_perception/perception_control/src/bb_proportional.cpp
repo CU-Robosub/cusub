f505b5784cb124844c0dd61f10160817ca47c8d6
Luke <lukebarbier12@gmail.com>
2019-07-15 23:42:57 -0600

    Proportional controller loads params
    Former-commit-id: c09deca5af7ae63a4583813fb7ec93be42593593

--- cusub_perception/perception_control/src/bb_proportional.cpp
+++ cusub_perception/perception_control/src/bb_proportional.cpp
@@ -4,8 +4,16 @@ namespace perception_control
 {
     BBProportional::BBProportional(ros::NodeHandle& nh) : BBController(nh)
     {
-        // load rosparams
         ROS_INFO("loading BB Proportional");
+        std::string downcamNamespace = "perception_control/bb_controllers/proportional/downcam";
+        if ( !nh.getParam(downcamNamespace + "/drive_max_setpoint", downcam_drive_max_setpoint) ) { ROS_ERROR("BBProportional couldn't locate params"); abort(); }
+        nh.getParam(downcamNamespace + "/strafe_max_setpoint", downcam_strafe_max_setpoint);
+        nh.getParam(downcamNamespace + "/drive_maxout_pixel_dist", downcam_drive_maxout_pixel_dist);
+        nh.getParam(downcamNamespace + "/strafe_maxout_pixel_dist", downcam_strafe_maxout_pixel_dist);
+        std::cout << downcam_drive_max_setpoint << std::endl;
+        std::cout <<  downcam_strafe_max_setpoint << std::endl;
+        std::cout <<  downcam_drive_maxout_pixel_dist << std::endl;
+        std::cout <<  downcam_strafe_maxout_pixel_dist << std::endl;
     }
 
     void BBProportional::respond(int xdiff, int ydiff)
