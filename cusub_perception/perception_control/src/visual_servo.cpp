06390aad02bb88f96c670de705a656eb42c23646
Luke <lukebarbier12@gmail.com>
2019-07-15 22:03:37 -0600

    Visual Servo recieves action goal
    Former-commit-id: 0f17280d9b21856a2595135896ead04c08cb819c

--- cusub_perception/perception_control/src/visual_servo.cpp
+++ cusub_perception/perception_control/src/visual_servo.cpp
@@ -8,10 +8,12 @@ namespace perception_control
         ros::NodeHandle& nh = getMTNodeHandle();
         wayToggleClient = nh.serviceClient<waypoint_navigator::ToggleControl>("cusub_common/toggleWaypointControl");
         darknetSub = nh.subscribe("cusub_perception/darknet_ros/bounding_boxes", 1, &VisualServo::darknetCallback, this);
+        server = new vsServer(nh, "visual_servo", boost::bind(&VisualServo::execute, this, _1), false);
+        server->start();
     }
     void VisualServo::darknetCallback(const darknet_ros_msgs::BoundingBoxesConstPtr bbs)
     {
-        NODELET_INFO("Received bounding box");
+        // NODELET_INFO("Received bounding box");
         if( !controllingPids ) { return; }
     }
     bool VisualServo::controlPids(const bool takeControl)
@@ -27,6 +29,16 @@ namespace perception_control
         else { NODELET_ERROR("VisualServo unable to change control of PIDs"); }
         controllingPids = takeControl;
         return toggle_srv.response.success;
+    }
+
+    void VisualServo::execute(const perception_control::VisualServoGoalConstPtr goal)
+    {
+        NODELET_INFO("VisualServo received request.");
+        activeGoal = goal;
+        VisualServoResult result;
+        result.success = true;
+        server->setSucceeded(result);
+        ros::Duration(1.0).sleep(); // give the task code a second to update the waypoint nav
     }
 }
 PLUGINLIB_EXPORT_CLASS(perception_control::VisualServo, nodelet::Nodelet);
