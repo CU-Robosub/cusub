686646633bfa26668c0cd79c83e82220bfe7c7f7
Luke <lukebarbier12@gmail.com>
2019-07-16 20:27:03 -0600

    Visual Servo centers on the path and stays!
    Former-commit-id: 6cb1111f47e6af02069975b9fe36b8458fe5eaaf

--- cusub_perception/perception_control/src/bb_proportional.cpp
+++ cusub_perception/perception_control/src/bb_proportional.cpp
@@ -12,10 +12,39 @@ namespace perception_control
         nh.getParam(downcamNamespace + "/strafe/maxout_pixel_dist", downcam_strafe_maxout_pixel_dist);
     }
 
+    void BBProportional::configureByCamera(std::string camera)
+    {
+        ROS_INFO("Configuring camera");
+        x_max_setpoint = downcam_strafe_max_setpoint;
+        y_max_setpoint = downcam_drive_max_setpoint;
+        x_maxout_pixel_dist = downcam_strafe_maxout_pixel_dist;
+        y_maxout_pixel_dist = downcam_drive_maxout_pixel_dist;
+    }
+
     void BBProportional::respond(int xdiff, int ydiff)
     {
-        // std::cout << xdiff << std::endl;
-        // std::cout << ydiff << std::endl << std::endl;
-        ;
+        ydiff = - ydiff;
+        std_msgs::Float64 x_set, y_set;
+        // X movement calc
+        float x_slope = x_max_setpoint / ( (float)x_maxout_pixel_dist );
+        x_set.data = x_slope * xdiff;
+        if (abs(x_set.data) > x_max_setpoint) // check if we've exceeded our max setpoint range
+        {
+            x_set.data = x_set.data > 0 ? x_max_setpoint : -x_max_setpoint;
+        }
+        // Y Movement calc
+        float y_slope = y_max_setpoint / ( (float)y_maxout_pixel_dist );
+        y_set.data = y_slope * ydiff;
+        if (abs(y_set.data) > y_max_setpoint) // check if we've exceeded our max setpoint range
+        {
+            y_set.data = y_set.data > 0 ? y_max_setpoint : -y_max_setpoint;
+        }
+
+        // Transform points into what the current state is
+        x_set.data += *x_state;
+        y_set.data += *y_state;
+
+        x_pub->publish(x_set);
+        y_pub->publish(y_set);
     }
 }
