7a987f537d3378e7ac5a9fb852782ef14104d88c
Sub <sub@sub.com>
2019-07-18 14:23:03 -0600

    Changed actuator service to be able to fire torpedos
    Former-commit-id: 513a3a5de6095582364ea702c7080e968473d58a

--- cusub_common/drivers/actuator/scripts/ActuatorService.py
+++ cusub_common/drivers/actuator/scripts/ActuatorService.py
@@ -3,10 +3,12 @@ import serial
 import time
 import rospy
 
+from actuator.srv import ActivateActuator
+
 class ActuatorService():
 
     def __init__(self):
-        self.ser = serial.Serial('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AI02RNUO-if00-port0', 9600)
+        self.ser = serial.Serial('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AI02RNUO-if00-port0', 115200)
 
     def activateActuator(self, req):
 
@@ -15,15 +17,24 @@ class ActuatorService():
 
         rospy.loginfo("Turning on acuator %d for %dms" % (pin, timeOn))
 
-        pin += timeOn
+        #pin += timeOn
         # I'm not 100% how this code worked, but for now for I will do following
         # <ActuatorNumber><Time_miliseconds>
         # ex Actuator #5 for 210ms write the folloing to serial port
         # 5210
 
-        cmd_str = str(pin)+str(timeOn)
+        #cmd_str = ""#str(pin)+str(timeOn)
+        #self.ser.write(cmd_str.encode())
 
-        self.ser.write(cmd_str.encode())
+	rospy.loginfo("Fire")
+	if pin == 2:
+		rospy.loginfo("2")
+		self.ser.write("\x33".encode())
+	elif pin == 3:
+		rospy.loginfo("3")
+		self.ser.write("D")
+
+	return []
 
     def run(self):
 
