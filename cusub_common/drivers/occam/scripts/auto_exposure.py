dfe99f839229571456457c0215d5535803dadfbf
Sub <sub@sub.com>
2019-07-31 11:48:40 -0600

    poolside changes
    Former-commit-id: 90b5c23d3e0562762436673128155bc63f955f3b

--- cusub_common/drivers/occam/scripts/auto_exposure.py
+++ cusub_common/drivers/occam/scripts/auto_exposure.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python
 
-# cusub_commom/occam/set_exposure
-
-#msg type Float 64
-
-#1000 is ususally decent
-# 2000 gets really bright
-#500 <x < 2000
 import dynamic_reconfigure.client
 import rospy
 from std_msgs.msg import Float64
 
 RANGE_LO = rospy.get_param('occam/range_lo')
 RANGE_HI = rospy.get_param('occam/range_hi')
+RANGE = RANGE_HI - RANGE_LO
 NUM_UPDATES = rospy.get_param('occam/num_updates')
 PERIOD = rospy.get_param('occam/update_period')
 INCREMENT = (RANGE_HI-RANGE_LO) / NUM_UPDATES
 STEP_WAIT = PERIOD / NUM_UPDATES
 
-set_exp_pub = rospy.Publisher("/cusub_common/occam/set_exposure", Float64, queue_size=1)
-exp_set = RANGE_LO
+set_exp_pub = rospy.Publisher("leviathan/set_exposure", Float64, queue_size=1)
+exp_set = RANGE_LO - INCREMENT
 
 rospy.init_node('auto_exposure', anonymous=True)
 # TODO: add node names for the cameras
-# downcam = dynamic_reconfigure.client.Client()
+# downcam = dynamic_reconfigure.client.Client("/camera/camera_nodelet/exposure")
 # torpcam = dynamic_reconfigure.client.Client()
 
 while not rospy.is_shutdown():
@@ -34,7 +28,7 @@ while not rospy.is_shutdown():
     msg.data = exp_set
     set_exp_pub.publish(msg)
     # TODO: create dict for downcam
-    # params = { '' : exp_set}
+    # params = { '' : exp_set/RANGE}
     # downcam.update_configuration(params)
     # TODO: update for torpcam
     # params = { '' : exp_set}
