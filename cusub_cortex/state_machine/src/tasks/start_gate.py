6c3a045593e990c3700b25faa86dba97028fe768
Derek Wright <derek.wright@colorado.edu>
2019-07-31 18:04:05 -0700

    wednesdev init, new startgate init

--- cusub_cortex/state_machine/src/tasks/start_gate.py
+++ cusub_cortex/state_machine/src/tasks/start_gate.py
@@ -21,6 +21,7 @@ import tf
 
 class StartGate(Task):
     name = "start_gate"
+    is_three_leg = False
 
     def __init__(self):
         super(StartGate, self).__init__() # become a state machine first
@@ -52,6 +53,8 @@ class Attack(Objective):
         self.replan_threshold = rospy.get_param('tasks/start_gate/replan_thresh', 1.0)
         self.leg_adjustment_meters = rospy.get_param('tasks/start_gate/third_leg_adjustment', 0.5)
         self.style_dist = rospy.get_param('tasks/start_gate/style_dist', 0.5)
+        self.three_leg_dist = rospy.get_param('tasks/start_gate/three_leg_dist', 1.5)
+        self.three_leg_depth = rospy.get_param('tasks/start_gate/three_leg_depth', 1.0)
         self.do_style = rospy.get_param('tasks/start_gate/do_style', False)
         self.spin_carrot = rospy.get_param('tasks/start_gate/spin_carrot_rads', 0.3)
         self.first_pose_received = False
@@ -59,6 +62,7 @@ class Attack(Objective):
         self.small_leg_left_side = None
         self.started = False
         self.current_yaw = None
+        self.current_depth = None
         self.yaw_pub = rospy.Publisher("cusub_common/motor_controllers/pid/yaw/setpoint", Float64, queue_size=10)
         rospy.loginfo("...waiting for cusub_common/toggleWaypointControl")
         rospy.wait_for_service("cusub_common/toggleWaypointControl")
@@ -66,12 +70,18 @@ class Attack(Objective):
         rospy.Subscriber("cusub_cortex/mapper_out/start_gate", PoseStamped, self.start_gate_callback)
         rospy.Subscriber("cusub_perception/start_gate/small_pole_left_side", Bool, self.small_leg_callback)
         rospy.Subscriber("cusub_common/motor_controllers/pid/yaw/state", Float64, self.yaw_callback)
-        
+        # add depth knowledge
+        self.depth_pub = rospy.Publisher("cusub_common/motor_controllers/pid/depth/setpoint", Float64, queue_size=1)
+        self.depth_sub = rospy.Subscriber("cusub_common/motor_controllers/pid/depth/state", Float64, self.depth_callback)
+
     def yaw_callback(self, msg):
         self.current_yaw = msg.data
 
+    def depth_callback(self,msg):
+        self.current_depth = msg.data
+
     def small_leg_callback(self, msg):
-        rospy.loginfo_throttle(0.0001, "Received left leg side.")
+        rospy.loginfo_throttle(0.0001, "Received third leg side.")
         if self.started and self.small_leg_left_side == None: # Receiving leg for the first time
             self.request_replan()
         elif self.started and self.small_leg_left_side != msg.data: # leg has switched sides!
@@ -110,10 +120,12 @@ class Attack(Objective):
                 else: # Replan has been requested loop again
                     pass
             else: # Pose reached successfully!
+                self.is_three_leg = True
                 break
 
         rospy.loginfo("...reached style pose")
         self.enact_style()
+        target_pose.position.z = self.three_leg_depth
         if self.go_to_pose(target_pose, userdata.timeout_obj, replan_enabled=False):
             userdata.outcome = "timed_out"
             return "timed_out"
@@ -130,6 +142,19 @@ class Attack(Objective):
                 self.small_leg_left_side, \
                 self.leg_adjustment_meters)
 
+            #three leg pose is the same concept as style pose. Reusing funciton
+            dist_in_front_of_gate = self.three_leg_dist + self.dist_behind
+            three_leg_pose = self.get_style_pose(self.cur_pose, target_pose, dist_in_front_of_gate)
+
+            if self.go_to_pose(three_leg_pose, userdata.timeout_obj):
+                if userdata.timeout_obj.timed_out:
+                    userdata.outcome = "timed_out"
+                    return "timed_out"
+            else:
+                self.is_three_leg = True
+
+            target_pose.position.z = self.three_leg_depth
+
             if self.go_to_pose(target_pose, userdata.timeout_obj):
                 if userdata.timeout_obj.timed_out:
                     userdata.outcome = "timed_out"
@@ -143,11 +168,31 @@ class Attack(Objective):
     def execute(self, userdata):
         self.started = True
         rospy.loginfo("Executing Attack")
-        self.configure_darknet_cameras([1,1,0,0,1,0])        
+        self.configure_darknet_cameras([1,1,0,0,1,0])
         if self.do_style:
             return self.do_gate_with_style(userdata)
         else:
             return self.do_gate_no_style(userdata)
+    #
+    # def go_thru_small_side(self):
+    #     '''
+    #     Responsibilities:
+    #         - adjust pose using third leg (should be taken care of by is_three_leg flag)
+    #         - set depth to proper value
+    #         - strafe to line up with new pose
+    #         - go_to_target
+    #     '''
+    #     # update startgate pose
+    #     target_pose = self.adjust_gate_pose(
+    #             self.cur_pose, \
+    #             self.start_gate_pose.pose, \
+    #             self.dist_behind, \
+    #             self.small_leg_left_side, \
+    #             self.leg_adjustment_meters)
+    #
+    #     # TODO: figure out how to line up
+
+        #depth_pub.publish()
 
     def enact_style(self):
         if self.current_yaw == None:
@@ -218,7 +263,7 @@ class Attack(Objective):
     @staticmethod
     def adjust_gate_pose(cur_pose, gate_pose, dist_behind, small_leg_left_side, third_leg_adjustment):
         """
-        Draws a line from the sub to the gate and adjusts the gate's pose along this line, behind the gate, by 
+        Draws a line from the sub to the gate and adjusts the gate's pose along this line, behind the gate, by
         'dist_behind'. Likewise, if small_leg_left_side is not None, a perpendicular line to the first is created
         and according to whichever side the small leg is on, the gate's pose is adjusted laterally by 'third_leg_adjustment'
         ( Assumes gate is approximately facing the sub. )
@@ -241,28 +286,30 @@ class Attack(Objective):
             gate_pose.position.x += 0.1
         if ( round(cur_pose.position.y, 2) == round(gate_pose.position.y,2) ): # Avoid infinite slope in the polyfit
             gate_pose.position.y -= 0.1
-        
 
-        # Adjust the gate pose to go through small leg side
-        if small_leg_left_side != None:
-            x = np.array([cur_pose.position.x, gate_pose.position.x])
-            y = np.array([cur_pose.position.y, gate_pose.position.y])
-            m, b = np.polyfit(x,y,1)
-            if m == 0:
-                m = 0.00001
-            perp_m = - 1 / m
-            perp_b = gate_pose.position.y - perp_m * gate_pose.position.x
-            x_hat = np.sqrt( ( third_leg_adjustment**2) / (perp_m**2 + 1) )
-            if (gate_pose.position.y > cur_pose.position.y) and small_leg_left_side:
-                x_hat = - x_hat
-            elif (gate_pose.position.y < cur_pose.position.y) and not small_leg_left_side:
-                x_hat = - x_hat
-            x_new = gate_pose.position.x + x_hat
-            y_new = x_new*perp_m + perp_b
+        if not self.is_three_leg:
+            return;
         else:
-            rospy.logwarn("Start Gate SM didn't receive 3rd leg side")
-            x_new = gate_pose.position.x
-            y_new = gate_pose.position.y
+            # Adjust the gate pose to go through small leg side
+            if small_leg_left_side != None:
+                x = np.array([cur_pose.position.x, gate_pose.position.x])
+                y = np.array([cur_pose.position.y, gate_pose.position.y])
+                m, b = np.polyfit(x,y,1)
+                if m == 0:
+                    m = 0.00001
+                perp_m = - 1 / m
+                perp_b = gate_pose.position.y - perp_m * gate_pose.position.x
+                x_hat = np.sqrt( ( third_leg_adjustment**2) / (perp_m**2 + 1) )
+                if (gate_pose.position.y > cur_pose.position.y) and small_leg_left_side:
+                    x_hat = - x_hat
+                elif (gate_pose.position.y < cur_pose.position.y) and not small_leg_left_side:
+                    x_hat = - x_hat
+                x_new = gate_pose.position.x + x_hat
+                y_new = x_new*perp_m + perp_b
+            else:
+                rospy.logwarn("Start Gate SM didn't receive 3rd leg side")
+                x_new = gate_pose.position.x
+                y_new = gate_pose.position.y
 
         # Adjust gate pose behind the gate
         x2 = np.array([cur_pose.position.x, x_new])
